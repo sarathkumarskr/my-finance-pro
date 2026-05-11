68868c1158d4af79c0a42f07de9471ed11ad0e04
sarathkumarskr <sarathkumar200@gmail.com>
2026-05-11 15:53:48 +0400

    fix: add real firebase config + iOS PWA auth

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,22 +1,24 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import {
+  indexedDBLocalPersistence,
+  initializeAuth
+} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAl5_gHq7j_4BYwKdh6o_dbGrk5oUy4wNU',
-  authDomain: 'my-finance-pro-b115a.firebaseapp.com',
-  projectId: 'my-finance-pro-b115a',
-  storageBucket: 'my-finance-pro-b115a.firebasestorage.app',
-  messagingSenderId: '785519739079',
-  appId: '1:785519739079:web:d0275c5e58fa0fa2093ee2',
+  apiKey: "AIzaSyAl5_gHq7j_4BYwKdh6o_dbGrk5oUy4wNU",
+  authDomain: "my-finance-pro-phi.vercel.app",
+  projectId: "my-finance-pro-b115a",
+  storageBucket: "my-finance-pro-b115a.firebasestorage.app",
+  messagingSenderId: "785519739079",
+  appId: "1:785519739079:web:d0275c5e58fa0fa2093ee2"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const auth = initializeAuth(app, {
+  persistence: indexedDBLocalPersistence
+});
 
+export const db = getFirestore(app);
 export default app;
